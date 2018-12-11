bda737cd76e3bed596862d20af613197849bbc7d
tomaswww <twingord@gmail.com>
2018-12-11 18:16:27 -0300

    Added MCV to app js structure and divs to view on index html

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Model
+var diffOpt = ["easy peasy","not that simple","hmm","wow dude slow down"];
+
+
+// Controller
+
+function renderDif(){
+diffOpt
+
+}
+
+
+// Difficulty View
+var diffSelect = document.getElementById("diffOPtions");
+diffSelect.createElement("li");
+
+//  Game panel View
