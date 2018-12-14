5de4b897b7cff1cbce1bee85ea8861a36f08d12a
tomaswww <twingord@gmail.com>
2018-12-14 14:43:24 -0300

    configured the difficulty panel according to diff array and not in index html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Model
-var diffOpt = ["easy peasy", "not that simple", "hmm", "wow dude slow down"];
+var diffOpt = ["easy peasy", "not that simple", "wow dude slow down"];
 
 
 // Controller
@@ -9,7 +9,23 @@ var diffOpt = ["easy peasy", "not that simple", "hmm", "wow dude slow down"];
 
 // when difficulty gets selected: 1-panel should render to game diff; 2-display on sidenav should show actual level
 // add event listener to drop btn to renders
+function renderDif(array){
+return array.forEach(function(element){
+  var newDif = document.createElement("li");
+  newDif.id ="difLev";
+  newDif.innerHTML = element;
+  newDif.addEventListener("click",function(){
+    return difSelected();
+    renderGame();
+  });
+  document.getElementById("myDropdown").appendChild(newDif);
+})
+}
 
+// get the dificulty selected on the diffuculty panel
+
+
+// get the game render according to difficulty selected
 
 
 // game panel
@@ -18,5 +34,5 @@ var diffOpt = ["easy peasy", "not that simple", "hmm", "wow dude slow down"];
 
 
 // Difficulty View
-
+renderDif(diffOpt);
 //  Game panel View
